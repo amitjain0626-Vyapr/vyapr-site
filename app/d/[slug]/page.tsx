473182efcfcaa734e67ed944c324f127d5d7bc13
amitjain0626-Vyapr <amitjain0626@gmail.com>
2025-08-05 02:02:12 +0530

    fix: use @utils alias for supabase import

--- app/d/[slug]/page.tsx
+++ app/d/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import { createClient } from "../../../utils/supabase/server";
+import { createClient } from "@/utils/supabase/server";
 
 export const dynamic = "force-dynamic";
 
